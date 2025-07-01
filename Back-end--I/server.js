6afa582551ccc17c54267106ace22247257f5c14
Nayara N Soares <nnasaria@mundolivre.digital>
2025-07-01 12:05:30 -0300

    Primeira Entrega

--- Back-end--I/server.js
+++ Back-end--I/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import "dotenv/config";
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Servidor em: http://localhost:${port}`);
